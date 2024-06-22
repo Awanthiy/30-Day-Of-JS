d7f79a0960fe88cdba14b824ffc91e72dd0e5592
Awanthi Malawanage <121420903+Awanthiy@users.noreply.github.com>
2024-06-22 09:45:31 +0530

    Create Q5.js

---
+++ Day 01 /Q5.js
@@ -0,0 +1,13 @@
+// Declare variables with different data types
+let name = "Ava";
+let age = 23;
+let isRegistered = true;
+let selectedColor = null;
+let favoriteNumber; // This will be undefined
+
+// Check the data type of each variable using typeof
+console.log(typeof name);      // Output: string
+console.log(typeof age);        // Output: number
+console.log(typeof isRegistered); // Output: boolean
+console.log(typeof selectedColor); // Output: object (**Important note**)
+console.log(typeof favoriteNumber); // Output: undefined
