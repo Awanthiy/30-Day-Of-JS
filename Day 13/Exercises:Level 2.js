8df30c076275e834aa5a8bb83000361ce95e9428
Awanthi Malawanage <121420903+Awanthiy@users.noreply.github.com>
2024-07-05 10:15:32 +0530

    Create Exercises:Level 2.js

---
+++ Day 13/Exercises:Level 2.js
@@ -0,0 +1,8 @@
+// Assertion example
+console.assert(10 > 2 * 10, 'Assertion failed: 10 is not greater than 2 * 10');
+
+// Warning message example
+console.warn('This is a warning message.');
+
+// Error message example
+console.error('This is an error message.');
