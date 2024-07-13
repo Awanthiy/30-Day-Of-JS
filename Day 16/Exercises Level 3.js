7b94b46d18cf3943ab7096e195c65026066b499a
Awanthi Malawanage <121420903+Awanthiy@users.noreply.github.com>
2024-07-13 10:05:09 +0530

    Create Exercises Level 3.js

---
+++ Day 16/Exercises Level 3.js
@@ -0,0 +1,14 @@
+const txt = `{
+  "Alex": {
+    "email": "alex@alex.com",
+    "skills": [
+      "HTML",
+      "CSS",
+      "JavaScript"
+    ],
+    "age": 20,
+    "isLoggedIn": false,
+    "points": 30
+  },
+  "Asab": {
+    "email": "asab@as
