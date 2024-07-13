02ebff53271639793bef291e52616f9f8e4a00ab
Awanthi Malawanage <121420903+Awanthiy@users.noreply.github.com>
2024-07-13 09:54:16 +0530

    Create Exercises Level 1.js

---
+++ Day 15/Exercises Level 1.js
@@ -0,0 +1,44 @@
+class Animal {
+  constructor(name, age, color, legs) {
+    this.name = name;
+    this.age = age;
+    this.color = color;
+    this.legs = legs;
+  }
+
+  makeSound() {
+    console.log(`${this.name} makes a noise.`);
+  }
+}
+
+class Dog extends Animal {
+  constructor(name, age, color, legs, breed) {
+    super(name, age, color, legs); // Call parent constructor
+    this.breed = breed;
+  }
+
+  makeSound() {
+    console.log(`${this.name} barks!`);
+  }
+}
+
+class Cat extends Animal {
+  constructor(name, age, color, legs, furLength) {
+    super(name, age, color, legs); // Call parent constructor
+    this.furLength = furLength;
+  }
+
+  makeSound() {
+    console.log(`${this.name} meows!`);
+  }
+}
+
+// Create animal objects
+const animal = new Animal("Leo", 3, "Brown", 4);
+const dog = new Dog("Buddy", 2, "Black", 4, "Labrador");
+const cat = new Cat("Whiskers", 1, "White", 4, "Long");
+
+// Make them speak!
+animal.makeSound(); // Leo makes a noise.
+dog.makeSound();    // Buddy barks!
+cat.makeSound();    // Whiskers meows!
